a47e7d5ee6ac13c140625b4482c8e07e7045773b
ElonGates <59313695+ElonGaties@users.noreply.github.com>
2024-08-30 11:36:10 +0100

    Implement `find_richest_addresses` and `count_total_addresses`

--- service/src/query.rs
+++ service/src/query.rs
@@ -31,4 +31,47 @@ impl Query {
     pub async fn find_address(db: &DbConn, address: &str) -> Result<Option<addresses::Model>, DbErr> {
         Address::find().filter(addresses::Column::Address.eq(address)).one(db).await
     }
+
+    /// Fetches the richest addresses from the database
+    ///
+    /// # Arguments
+    /// * `db` - The database connection
+    /// * `limit` - The number of addresses to fetch
+    /// * `offset` - The offset for pagination
+    ///
+    /// # Examples
+    /// ```
+    /// let limit = 10;
+    /// let offset = 0;
+    /// let richest_addresses = Query::find_richest_addresses(db, limit, offset).await?;
+    ///
+    /// for (index, address) in richest_addresses.iter().enumerate() {
+    ///     println!("{}. Address: {}, Balance: {}", index, address.address, address.balance);
+    /// }
+    /// ```
+    pub async fn find_richest_addresses(db: &DbConn, limit: u64, offset: u64) -> Result<Vec<addresses::Model>, DbErr> {
+        Address::find()
+            .order_by_desc(addresses::Column::Balance)
+            .limit(limit)
+            .offset(offset)
+            .all(db)
+            .await
+    }
+
+    /// Counts the total number of addresses in the database
+    ///
+    /// # Arguments
+    /// * `db` - The database connection
+    ///
+    /// # Returns
+    /// The total number of addresses as a `u64`
+    ///
+    /// # Examples
+    /// ```
+    /// let total = Query::count_total_addresses(&db).await?;
+    /// println!("Total addresses: {}", total);
+    /// ```
+    pub async fn count_total_addresses(db: &DbConn) -> Result<u64, DbErr> {
+        Address::find().count(db).await
+    }
 }
