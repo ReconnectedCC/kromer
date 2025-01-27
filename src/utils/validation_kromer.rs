b1034dc1ffce6653ca793b62ebc7fa9abf4b3336
sovnya <enp7s1@tutanota.com>
2025-01-27 22:37:18 +0100

    fix: Use once_cell Lazy instead of making a new compiled regex each function call

--- src/utils/validation_kromer.rs
+++ src/utils/validation_kromer.rs
@@ -1,39 +1,44 @@
+use once_cell::sync::Lazy;
 use regex::Regex;
 
-const ADDRESS_RE_V2: &str = r"^k[a-z0-9]{9}$";
-const ADDRESS_LIST_RE: &str = r"^(?:k[a-z0-9]{9}|[a-f0-9]{10})(?:,(?:k[a-z0-9]{9}|[a-f0-9]{10}))*$";
-const NAME_FETCH_RE: &str = r"^(?:xn--)?[a-z0-9-_]{1,64}$";
-const NAME_RE: &str = r"^[a-z0-9_-]{1,64}$";
-const NAME_A_RECORD_RE: &str = r"^[^\s.?#].[^\s]*$";
-const _NAME_META_RE: &str = r"^(?:([a-z0-9-_]{1,32})@)?([a-z0-9]{1,64})\.kst$";
+static ADDRESS_RE_V2: Lazy<Regex> = Lazy::new(|| Regex::new(r"^k[a-z0-9]{9}$").unwrap());
+static ADDRESS_LIST_RE: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r"^(?:k[a-z0-9]{9}|[a-f0-9]{10})(?:,(?:k[a-z0-9]{9}|[a-f0-9]{10}))*$").unwrap()
+});
+static NAME_FETCH_RE: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"^(?:xn--)?[a-z0-9-_]{1,64}$").unwrap());
+static NAME_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[a-z0-9_-]{1,64}$").unwrap());
+static NAME_A_RECORD_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[^\s.?#].[^\s]*$").unwrap());
+static _NAME_META_RE: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"^(?:([a-z0-9-_]{1,32})@)?([a-z0-9]{1,64})\.kst$").unwrap());
 
+#[inline(always)]
 pub fn is_valid_name(name: &str, fetching: bool) -> bool {
-    let re = if fetching { NAME_FETCH_RE } else { NAME_RE };
-
-    // Gonna just unwrap and assume it's okay, just don't break Regex strings pls
-    let re = Regex::new(re).unwrap();
     let name = name.to_lowercase();
 
-    re.is_match(&name) && !name.is_empty() && name.len() <= 64
+    // Bit silly to do it like this but yolo lol
+    match fetching {
+        true => NAME_FETCH_RE.is_match(&name) && !name.is_empty() && name.len() <= 64,
+        false => NAME_RE.is_match(&name) && !name.is_empty() && name.len() <= 64,
+    }
 }
 
+#[inline(always)]
 pub fn is_valid_kromer_address(address: &str) -> bool {
-    let re = Regex::new(ADDRESS_RE_V2).unwrap();
-
-    re.is_match(address)
+    ADDRESS_RE_V2.is_match(address)
 }
 
+#[inline(always)]
 pub fn is_valid_kromer_address_list(address_list: &str) -> bool {
-    let re = Regex::new(ADDRESS_LIST_RE).unwrap();
-
-    re.is_match(address_list)
+    ADDRESS_LIST_RE.is_match(address_list)
 }
 
+#[inline(always)]
 pub fn is_valid_a_record(a: &str) -> bool {
-    let re = Regex::new(NAME_A_RECORD_RE).unwrap();
-    !a.is_empty() && a.len() <= 255 && re.is_match(a)
+    !a.is_empty() && a.len() <= 255 && NAME_A_RECORD_RE.is_match(a)
 }
 
+#[inline(always)]
 pub fn strip_name_suffix(name: &str) -> String {
     name.replace(r"\.kst$", "")
 }
